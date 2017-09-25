bd634c3a400d04798d6aa8c81ecab331fd7accff
musjassani <mjassani@gmail.com>
2017-09-25 12:41:15 +0200

    commit d'un conflit

--- src/main/java/com/amj/beans/Personne.java
+++ src/main/java/com/amj/beans/Personne.java
@@ -3,10 +3,12 @@ package com.amj.beans;
 public class personne {
 	private String nom;
 	
+	//getter
 	public void getNom() {
 		return nom;
 	}
 	
+	//setter
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
